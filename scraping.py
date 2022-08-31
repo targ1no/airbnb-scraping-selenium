d2ee204a1a8f6e11e126a78466164d038f0e746a
Bruna Targino <contatobrunatargino@gmail.com>
2022-08-31 11:15:09 -0300

    mudando estrutura do código

--- scraping.py
+++ scraping.py
@@ -7,64 +7,77 @@ import pandas as pd
 
 options = webdriver.ChromeOptions()
 options.add_argument('window-size=400,800')
-
-# Iniciando Chrome 
+ 
 browser = webdriver.Chrome(options=options)
 url = 'https://www.airbnb.com.br/'
-browser.get(url) 
-
-property_list = []
-
-# Interação/navegação com o site
-sleep(3)
-button_where = browser.find_element(By.TAG_NAME, 'button')
-button_where.click()
-
-sleep(2)
-input_search = browser.find_element(By.TAG_NAME, 'label')
-input_search.click()
-
-sleep(3)
-input_text = browser.find_element(By.ID, '/homes-1-input')
-input_text.send_keys('Recife')
-input_text.submit()
-
-sleep(2)
-next_button = browser.find_element(By.CLASS_NAME, '_1ku51f04')
-next_button.click()
-
-sleep(2)
-next_button2 = browser.find_elements(By.TAG_NAME, 'button')[-1]
-next_button2.click()
-
-sleep(3)
-
-# Pegando html pelo Selenium e transformando em obj. BS
-page_content = browser.page_source 
-site = BeautifulSoup(page_content, 'html.parser')
+browser.get(url)
 
 sleep(2)
 
-# Raspando dados da pesquisa
-properties = site.findAll('div', attrs={'itemprop': 'itemListElement'})
+class Scrappy:
 
-for _property in properties:
+    def init(self):
+        self.input_texting()
+        self.choosing_in_calendar()
+        self.qtt_people()
+        sleep(2)
+        self.scraping_datas()
+
+    def input_texting(self):
+
+        sleep(3)
+        button_where = browser.find_element(By.TAG_NAME, 'button')
+        button_where.click()
+
+        sleep(2)
+        input_search = browser.find_element(By.TAG_NAME, 'label')
+        input_search.click()
+
+        sleep(3)
+        input_text = browser.find_element(By.ID, '/homes-1-input')
+        input_text.send_keys('Recife')
+        input_text.submit()
+
+    def choosing_in_calendar(self):
+
+        sleep(2)
+        next_button = browser.find_element(By.CLASS_NAME, '_1ku51f04')
+        next_button.click()
+
+    def qtt_people(self):
+
+        sleep(2)
+        next_button2 = browser.find_elements(By.TAG_NAME, 'button')[-1]
+        next_button2.click()
+
+    def scraping_datas(self):
+
+        page_content = browser.page_source 
+        site = BeautifulSoup(page_content, 'html.parser')  
+
+        property_list = []
+
+        properties = site.findAll('div', attrs={'itemprop': 'itemListElement'})
+
+        for _property in properties:
     
-    prop_url = _property.find('meta', attrs={'itemprop': 'url'})
-    prop_url = prop_url['content']
+            prop_url = _property.find('meta', attrs={'itemprop': 'url'})
+            prop_url = prop_url['content']
 
-    prop_name = _property.find('div', attrs={'class': 't1jojoys dir dir-ltr'})
+            prop_name = _property.find('div', attrs={'class': 't1jojoys dir dir-ltr'})
 
-    prop_desc = _property.find('meta', attrs={'itemprop': 'name'})
-    prop_desc = prop_desc['content']
+            prop_desc = _property.find('meta', attrs={'itemprop': 'name'})
+            prop_desc = prop_desc['content']
 
-    night_prop_price = _property.find('span', attrs={'class': '_tyxjp1'})
+            night_prop_price = _property.find('span', attrs={'class': '_tyxjp1'})
 
-    # Adicionando resultado na lista
-    property_list.append([prop_url, prop_name.text, prop_desc, night_prop_price.text])
+            property_list.append([prop_url, prop_name.text, prop_desc, night_prop_price.text])
 
-# Salvando resultado no Excel
-property_list = pd.DataFrame(property_list, columns=['URL', 'Prop Name', 'Prop Desc', 'Price'])
-property_list.to_excel('properties.xlsx', index=False)
+        # Saving in Excel with Pandas
+        property_list = pd.DataFrame(property_list, columns=['Property link', 'Property name', 'Property description', 'Property price (night)'])
+        property_list.to_excel('properties.xlsx', index=False)
+        
+        print(property_list)
 
-print(property_list)
+start_scrappy = Scrappy()
+start_scrappy.init()
